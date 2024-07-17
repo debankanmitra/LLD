c89dd0bf365d05fcfb7528ecc88d5af4fe8c61e1
debankanmitra <debankanmitra@gmail.com>
2024-07-17 13:48:10 +0530

    decorators

--- decorators.py
+++ decorators.py
@@ -1,5 +1,5 @@
 import time
-def decorator(func):
+def time_it(func):
     def inner(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
@@ -7,7 +7,7 @@ def decorator(func):
         print(func.__name__,"tooks", str((end - start)*1000) , "miliseconds to execute")
     return inner
 
-@decorator
+@time_it
 def calcsum(numbers):
     sum = 0
     for num in numbers:
@@ -16,3 +16,19 @@ def calcsum(numbers):
 
 array = range(1,900000)
 calcsum(array)
+
+# ==============================================================================================================
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Something is happening before the function is called.")
+        result = func(*args, **kwargs)
+        print("Something is happening after the function is called.")
+        return result
+    return wrapper
+
+@my_decorator
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
